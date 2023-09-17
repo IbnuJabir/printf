2cf15f87e3f1dae9059d1bc868065516c4a4de03
IbnuJabir <kedirjabir12@gmail.com>
2023-09-17 13:23:21 -0700

    second commit for _print function

--- _printf.c
+++ _printf.c
@@ -24,5 +24,48 @@ int print_char(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * _printf - this is a custom printf function that writes characters on
+ * the standard output
+ * @format: format specifier string
+ * Return: number of characters printed to the stdout
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	char *str = NULL;
+	va_list vargs;
 
+	va_start(vargs, format);
+
+	/* iterate format string up to the last non-null character */
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			count += print_char(*format);
+		}
+		else
+		{
+			/* move to the next char after % */
+			++format;
+
+			/* check if character after % is c or s */
+			if (*format == 'c')
+			{
+				count += print_char(va_arg(vargs, int));
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(vargs, char *);
+				printf("(%s)", str);
+				count += print_str(str);
+			}
+		}
+
+	format++;
+	}
+	va_end(vargs);
+	return (count);
+}
 
