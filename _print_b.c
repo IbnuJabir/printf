7f84b52d3112d3df3fbadbe119f5c0af34efaecf
julioceaseless <julioceaseless@gmail.com>
2023-09-18 18:13:23 +0000

    revert

--- _print_b.c
+++ _print_b.c
@@ -4,14 +4,12 @@
  * @num: The unsigned integer to be printed.
  * Return: The number of characters printed.
  */
-unsigned int print_binary(int num)
+unsigned int print_binary(unsigned int num)
 {
 	unsigned int count = 0;
 	unsigned int bit = 0;
 	int has_zeros = 1;
 
-	if (num < 0)
-		return (-1);
 	bit = 1 << (sizeof(num) * 8 - 1);
 	for (; bit > 0; bit >>= 1)
 	{
