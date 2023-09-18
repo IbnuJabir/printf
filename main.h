eb99bc9d8fa78ad649909e5ecb79690cb76b30a9
IbnuJabir <kedirjabir12@gmail.com>
2023-09-18 15:34:42 -0700

    add more functions for u,o,x,X specifiers and update the printf.c file

--- main.h
+++ main.h
@@ -7,6 +7,17 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int rot_13(char *str);
+int print_number(int n);
+int print_str(char *str);
+
 int print_binary(unsigned int num);
+int print_unsign(unsigned int num);
+int print_octal(unsigned int num);
+int print_hex(unsigned int num, int uppercase);
+
+int handle_string(va_list args);
+int handle_rot13(va_list args);
+int handle_default(char f_selector);
 
 #endif
