36490253234f89f1e240f9280e9e5db56a21cdb7
julioceaseless <julioceaseless@gmail.com>
2023-09-18 18:11:09 +0000

    error handling for negative numbers

--- _print_b.c
+++ _print_b.c
@@ -2,15 +2,17 @@
 /**
  * print_binary - Prints the binary representation of an unsigned integer.
  * @num: The unsigned integer to be printed.
- *
- * Returns: The number of characters printed.
+ * Return: The number of characters printed.
  */
-unsigned int print_binary(unsigned int num)
+unsigned int print_binary(int num)
 {
 	unsigned int count = 0;
-	unsigned int bit = 1 << (sizeof(num) * 8 - 1);
+	unsigned int bit = 0;
 	int has_zeros = 1;
 
+	if (num < 0)
+		return (-1);
+	bit = 1 << (sizeof(num) * 8 - 1);
 	for (; bit > 0; bit >>= 1)
 	{
 		if (num & bit)
